23e20762f6e3f73f93b207809d94fd4e32c10279
Otto Meszaros <otto.meszaros@carouselgroup.net>
2022-04-19 22:09:28 +0200

    Added cost based test

--- src/test/scala/MinTrianglePathSpec.scala
+++ src/test/scala/MinTrianglePathSpec.scala
@@ -26,4 +26,24 @@ class MinTrianglePathSpec extends AnyFlatSpec with Matchers {
       rightCost = 3)
     traverseTreeBreadthFirst(sampleTree)(Tree.children).map(_.value).toList shouldBe List(0, 1, 1, 2, 2, 2, 2)
   }
+
+  "Cost update" should "update the values" in {
+    val sampleTree: Tree = Tree.Branch(
+      value = 0,
+      left = Tree.Branch(
+        value = 1,
+        left = Tree.Leaf(2),
+        leftCost = 0,
+        right = Tree.Leaf(2),
+        rightCost = 0),
+      leftCost = 6,
+      right = Tree.Branch(
+        value = 1,
+        left = Tree.Leaf(2),
+        leftCost = 0,
+        right = Tree.Leaf(2),
+        rightCost = 0),
+      rightCost = 3)
+    traverseTreeBreadthFirst(sampleTree)(Tree.children).map(_.value).toList shouldBe List(0, 6, 3, 9, 14, 11, 8)
+  }
 }
